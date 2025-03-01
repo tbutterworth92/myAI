abf66e34e421fed1b9e877acc79b8d8a91aafdea
tbutterworth92 <trentbutterworth@gmail.com>
2025-03-01 11:07:37 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -2,8 +2,8 @@ import { AI_NAME, OWNER_NAME} from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `[LAPIS AI]`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `[FOOTER MESSAGE]`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
+export const FOOTER_MESSAGE: string = `[سلام ملګری/سلام دوست]`;
+export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON]`;
 export const PAGE_TITLE: string = `[LAPIS AI]`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
